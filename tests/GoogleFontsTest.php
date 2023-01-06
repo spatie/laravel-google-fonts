0a4d9b3c65ee908a995c6f0752849268e3bd96ea
Alex Manase <malexandru699@gmail.com>
2023-01-06 17:31:40 +0200

    refactor: GoogleFontsTest

--- tests/GoogleFontsTest.php
+++ tests/GoogleFontsTest.php
@@ -1,59 +1,52 @@
 <?php
 
-namespace Spatie\GoogleFonts\Tests;
-
 use Illuminate\Support\Str;
 use Spatie\GoogleFonts\GoogleFonts;
-use Spatie\Snapshots\MatchesSnapshots;
 
-class GoogleFontsTest extends TestCase
-{
-    use MatchesSnapshots;
+use function Spatie\Snapshots\assertMatchesFileSnapshot;
+use function Spatie\Snapshots\assertMatchesHtmlSnapshot;
 
-    /** @test */
-    public function it_loads_google_fonts()
-    {
-        $fonts = app(GoogleFonts::class)->load('inter', forceDownload: true);
+it('loads google fonts', function () {
+    $fonts = app(GoogleFonts::class)->load('inter', forceDownload: true);
 
-        $expectedFileName = '952ee985ef/fonts.css';
+    $expectedFileName = '952ee985ef/fonts.css';
 
-        $this->disk()->assertExists($expectedFileName);
+    $this->disk()->assertExists($expectedFileName);
 
-        $fullCssPath = $this->disk()->path($expectedFileName);
+    $fullCssPath = $this->disk()->path($expectedFileName);
 
-        $this->assertMatchesFileSnapshot($fullCssPath);
+    assertMatchesFileSnapshot($fullCssPath);
 
-        $woff2FileCount = collect($this->disk()->allFiles())
-            ->filter(fn (string $path) => Str::endsWith($path, '.woff2'))
-            ->count();
+    $woff2FileCount = collect($this->disk()->allFiles())
+        ->filter(fn (string $path) => Str::endsWith($path, '.woff2'))
+        ->count();
 
-        $this->assertGreaterThan(0, $woff2FileCount);
+    expect($woff2FileCount)->toBeGreaterThan(0);
 
-        $this->assertMatchesHtmlSnapshot((string)$fonts->link());
-        $this->assertMatchesHtmlSnapshot((string)$fonts->inline());
+    assertMatchesHtmlSnapshot((string)$fonts->link());
+    assertMatchesHtmlSnapshot((string)$fonts->inline());
 
-        $expectedUrl = $this->disk()->url($expectedFileName);
-        $this->assertEquals($expectedUrl, $fonts->url());
-    }
+    $expectedUrl = $this->disk()->url($expectedFileName);
+    expect($fonts->url())->toEqual($expectedUrl);
+});
 
-    /** @test */
-    public function it_falls_back_to_google_fonts()
-    {
-        config()->set('google-fonts.fonts', ['cow' => 'moo']);
-        config()->set('google-fonts.fallback', true);
+it('falls back to google fonts', function () {
+    config()->set('google-fonts.fonts', ['cow' => 'moo']);
+    config()->set('google-fonts.fallback', true);
 
-        $fonts = app(GoogleFonts::class)->load('cow', forceDownload: true);
+    $fonts = app(GoogleFonts::class)->load('cow', forceDownload: true);
 
-        $allFiles = $this->disk()->allFiles();
+    $allFiles = $this->disk()->allFiles();
 
-        $this->assertCount(0, $allFiles);
+    expect($allFiles)->toHaveCount(0);
 
-        $fallback = <<<HTML
+    $fallback = <<<HTML
             <link href="moo" rel="stylesheet" type="text/css">
         HTML;
 
-        $this->assertEquals($fallback, (string)$fonts->link());
-        $this->assertEquals($fallback, (string)$fonts->inline());
-        $this->assertEquals('moo', $fonts->url());
-    }
-}
+    expect([
+        (string)$fonts->link(),
+        (string)$fonts->inline()
+    ])->each->toEqual($fallback)
+        ->and($fonts->url())->toEqual('moo');
+});
