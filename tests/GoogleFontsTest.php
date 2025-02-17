08a9e58d68ab27b439282787dcc77c166cb81b1a
Freek Van der Herten <freek@spatie.be>
2025-02-17 10:41:23 +0100

    fix tests

--- tests/GoogleFontsTest.php
+++ tests/GoogleFontsTest.php
@@ -6,8 +6,8 @@ use Spatie\GoogleFonts\GoogleFonts;
 use function Spatie\Snapshots\assertMatchesFileSnapshot;
 use function Spatie\Snapshots\assertMatchesHtmlSnapshot;
 
-it('loads google fonts', function ($arguments) {
-    $fonts = app(GoogleFonts::class)->load($arguments, forceDownload: true);
+it('loads google fonts as string', function () {
+    $fonts = app(GoogleFonts::class)->load('inter', forceDownload: true);
 
     $expectedFileName = '952ee985ef/fonts.css';
 
@@ -28,16 +28,37 @@ it('loads google fonts', function ($arguments) {
 
     $expectedUrl = $this->disk()->url($expectedFileName);
     expect($fonts->url())->toEqual($expectedUrl);
-})->with([
-    'font_as_string' => 'inter',
-    'font_as_array' => ['font' => 'inter'],
-]);
+});
 
-it('falls back to google fonts', function ($arguments) {
+it('loads google fonts as array', function () {
+    $fonts = app(GoogleFonts::class)->load(['font' => 'inter'], forceDownload: true);
+
+    $expectedFileName = '952ee985ef/fonts.css';
+
+    $this->disk()->assertExists($expectedFileName);
+
+    $fullCssPath = $this->disk()->path($expectedFileName);
+
+    assertMatchesFileSnapshot($fullCssPath);
+
+    $woff2FileCount = collect($this->disk()->allFiles())
+        ->filter(fn (string $path) => Str::endsWith($path, '.woff2'))
+        ->count();
+
+    expect($woff2FileCount)->toBeGreaterThan(0);
+
+    assertMatchesHtmlSnapshot((string) $fonts->link());
+    assertMatchesHtmlSnapshot((string) $fonts->inline());
+
+    $expectedUrl = $this->disk()->url($expectedFileName);
+    expect($fonts->url())->toEqual($expectedUrl);
+});
+
+it('falls back to google fonts with a string argument', function () {
     config()->set('google-fonts.fonts', ['cow' => 'moo']);
     config()->set('google-fonts.fallback', true);
 
-    $fonts = app(GoogleFonts::class)->load($arguments, forceDownload: true);
+    $fonts = app(GoogleFonts::class)->load('cow', forceDownload: true);
 
     $allFiles = $this->disk()->allFiles();
 
@@ -52,12 +73,29 @@ it('falls back to google fonts', function ($arguments) {
         (string) $fonts->inline(),
     ])->each->toEqual($fallback)
         ->and($fonts->url())->toEqual('moo');
-})
-    ->with([
-        'font_as_string' => 'cow',
-        'font_as_array' => ['font' => 'cow'],
-    ]);
-;
+});
+
+it('falls back to google fonts with a array argument', function () {
+    config()->set('google-fonts.fonts', ['cow' => 'moo']);
+    config()->set('google-fonts.fallback', true);
+
+    $fonts = app(GoogleFonts::class)->load(['font' => 'cow'], forceDownload: true);
+
+    $allFiles = $this->disk()->allFiles();
+
+    expect($allFiles)->toHaveCount(0);
+
+    $fallback = <<<HTML
+            <link href="moo" rel="stylesheet" type="text/css">
+        HTML;
+
+    expect([
+        (string) $fonts->link(),
+        (string) $fonts->inline(),
+    ])->each->toEqual($fallback)
+        ->and($fonts->url())->toEqual('moo');
+});
+
 
 it('adds the nonce attribute when specified', function () {
     config()->set('google-fonts.fonts', ['cow' => 'moo']);
